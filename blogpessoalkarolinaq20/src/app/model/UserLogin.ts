7d04aa2ccd3d44300055e923d23aa756d035cc67
karolinaq20 <karolinaq20@hotmail.com>
2021-06-29 23:31:32 -0300

    Cosumo da API parte-1

---
+++ blogpessoalkarolinaq20/src/app/model/UserLogin.ts
@@ -0,0 +1,9 @@
+export class UuserLogin{
+    public id:number
+    public nome: string
+    public usuario: string
+    public senha: string
+    public token: string
+    public foto: string
+    public tipo: string
+}
